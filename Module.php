ea0b693595cd110dc9b781b8c72a8ac9041b9ff0
Carsten Bleek <bleek@cross-solution.de>
2015-08-19 13:09:09 +0200

    adds flags and bootstrap-social

--- Module.php
+++ Module.php
@@ -19,9 +19,9 @@ class Module
 
 
     function getConfig()
-   {
-       return include __DIR__ . '/config/module.config.php';
-   }
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
 
     function onBootstrap()
     {
