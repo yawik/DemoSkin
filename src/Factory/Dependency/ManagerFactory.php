c8639f53e70be81f89a722c6bd034f9ee04a250b
Mathias Gelhausen <mathias@xenon>
2016-09-06 14:05:43 +0200

    Prevent standard user accounts removal.

---
+++ src/Factory/Dependency/ManagerFactory.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Yawik DemoSkin
+ */
+
+/**  */
+namespace YawikDemoSkin\Factory\Dependency;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ *
+ *
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
+ */
+class ManagerFactory implements FactoryInterface
+{
+
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $manager = new \YawikDemoSkin\Dependency\Manager($serviceLocator->get('Core/DocumentManager'));
+        $manager->setEventManager($serviceLocator->get('Auth/Dependency/Manager/Events'));
+        
+        return $manager;
+    }
+}
