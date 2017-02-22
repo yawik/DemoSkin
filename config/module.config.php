e8739120df184d890fadb5871c60b4639c37711d
Carsten Bleek <bleek@cross-solution.de>
2017-02-22 20:53:19 +0100

    removes old way of doing classification

--- config/module.config.php
+++ config/module.config.php
@@ -20,7 +20,6 @@ return array(
                      'core/index/index' => __DIR__ . '/../view/index.phtml',
                      'auth/password/index' => __DIR__ . '/../view/password.phtml',
                      'piwik' => __DIR__ . '/../view/piwik.phtml',
-                     'iframe/iFrame.phtml'               => __DIR__ . '/../view/iFrame.phtml',
                      'jobs/jobboard/index.ajax.phtml' => __DIR__ . '/../view/jobs/index.ajax.phtml',
                      'auth/users/list.ajax.phtml' => __DIR__ . '/../view/auth/users/list.ajax.phtml', // hide email adresses, since this is is a public demo
                       ),
@@ -38,11 +37,7 @@ return array(
 
              'form_elements' => [
                  'invokables' => [
-                     'YawikDemoSkin/ClassificationForm' => 'YawikDemoSkin\Form\ClassificationForm',
                      'Jobs/Description' => 'YawikDemoSkin\Form\JobsDescription',
-                 ],
-                 'factories' => [
-                     'YawikDemoSkin/ClassificationFieldset' => 'YawikDemoSkin\Form\ClassificationFieldsetFactory',
                  ],
              ],
 
