708fe549ff2ddb4770b6e6106f02c3eeea40a075
Carsten Bleek <bleek@cross-solution.de>
2019-09-04 19:19:49 +0200

    adds deployer

---
+++ deploy.php
@@ -0,0 +1,45 @@
+<?php
+namespace Deployer;
+
+require 'recipe/zend_framework.php';
+
+// Project name
+set('application', '137.74.70.56');
+
+// Project repository
+set('repository', 'https://github.com/yawik/DemoSkin.git');
+
+// Shared files/dirs between deploys 
+add('shared_files', [
+    'test/sandbox/public/.htaccess',
+    'test/sandbox/public/robots.txt',
+]);
+
+add('shared_dirs', [
+    'test/sandbox/var/log',
+    'test/sandbox/var/cache',
+    'test/sandbox/config/autoload',
+    'test/sandbox/public/static',	// used by eg. organization logos
+]);
+
+// Writable dirs by web server 
+add('writable_dirs', [
+    'test/sandbox/var/cache',
+    'test/sandbox/var/log',
+    'test/sandbox/public/static',
+]);
+
+set('default_stage', 'prod');
+
+// Hosts
+
+host('137.74.70.56')
+    ->user('yawik')
+    ->stage('prod')
+    ->multiplexing(false) 
+    ->set('deploy_path', '/var/www/production')
+    ->set('writableusesudo', true);   
+    
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
